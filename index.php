ca7e33166bb5f77c75b8a2a10efc70f14e881eed
andreathepa <andreadepa32@gmail.com>
2023-07-11 14:22:27 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+    require_once __DIR__."/Models/Product.php";
+    require_once __DIR__."/Models/Food.php";
+    require_once __DIR__."/Models/Games.php";
+    require_once __DIR__."/Models/Utilities.php";
+
+    // $food_1 = new Food ('https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Royal Canin Mini Adult', 'Euro 43,99','Dog', '545', 'prosciutto, riso');
+
+    // $food_2 = new Food ('https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpghttps://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Almo Nature Holistic Maintenance Medium Large', 'Euro 44,99','Dog','600', 'manzo, cereali');
+
+    // $food_3 = new Food ('https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpghttps://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Almo Nature Cat Daily Lattina', 'Euro 34,99','Cat','400', 'tonno, pollo, prosciutto');
+
+    // $food_4 = new Food ('https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpghttps://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Mangime per pesci Guppi', 'Euro 2,99','Fish','30', 'Pesci, alghe, cereali');
+
+    // $Utilities_1 = new Utilities ('https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Voliera Wilma in Legno', 'Euro 184,99','Bird', 'Wood', '120 cm x 200 cm');
+
+    // $Utilities_2 = new Utilities ('https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpghttps://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Cartucce filtranti per fitro pesci', 'Euro 2,99','Fish','Materiale espanso', 'ND');
+
+    // $Games_1 = new Games ('https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpghttps://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Kong Classic', 'Euro 13,99','Dog','Galleggia e rimbalza', '8,5 cm x 10cm');
+
+    // $Games_2 = new Games ('https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpghttps://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Topini', 'Euro 4,99','Cat','Morbido con codina', 'Pesci, alghe, cereali');
+
+
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style/style.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+        <title>Php OOP 3</title>
+    </head>
+    <body>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="content">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
